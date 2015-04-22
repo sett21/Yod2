859ffc61aebe55dcc7aa7975ebf9a91a377102a8
Alexander <alexandr.trubachev@gns-it.com>
2015-04-22 11:51:15 +0300

    Change preloader

--- protected/views/design/index.php
+++ protected/views/design/index.php
@@ -14,7 +14,52 @@
     <script src="/themes/front/js/jq.js"></script>
 </head>
 <body>
-<div class="loading"></div>
+<div class="loading">
+    <svg viewBox="0 0 120 120" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+      <symbol id="s--circle">
+        <circle r="10" cx="20" cy="20"></circle>
+      </symbol>
+      
+      <g class="g-circles g-circles--v1">
+        <g class="g--circle">
+          <use xlink:href="#s--circle" class="u--circle"/>
+        </g>  
+        <g class="g--circle">
+          <use xlink:href="#s--circle" class="u--circle"/>
+        </g>
+        <g class="g--circle">
+          <use xlink:href="#s--circle" class="u--circle"/>
+        </g>
+        <g class="g--circle">
+          <use xlink:href="#s--circle" class="u--circle"/>
+        </g>
+        <g class="g--circle">
+          <use xlink:href="#s--circle" class="u--circle"/>
+        </g>
+        <g class="g--circle">
+          <use xlink:href="#s--circle" class="u--circle"/>
+        </g>
+        <g class="g--circle">
+          <use xlink:href="#s--circle" class="u--circle"/>
+        </g>
+        <g class="g--circle">
+          <use xlink:href="#s--circle" class="u--circle"/>
+        </g>
+        <g class="g--circle">
+          <use xlink:href="#s--circle" class="u--circle"/>
+        </g>
+        <g class="g--circle">
+          <use xlink:href="#s--circle" class="u--circle"/>
+        </g>
+        <g class="g--circle">
+          <use xlink:href="#s--circle" class="u--circle"/>
+        </g>
+        <g class="g--circle">
+          <use xlink:href="#s--circle" class="u--circle"/>
+        </g>
+      </g>
+  </svg>    
+</div>
 <!-- �������� ����� ��� ���� ������� -->
 <button id="menu-trigger"></button>
 <!-- ���� -->
